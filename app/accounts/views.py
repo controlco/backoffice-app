61620e3a3ea9c551021cb9cb70c05c89c9954e7e
chris-carstens <ctcarstens@uc.cl>
2021-04-27 03:37:29 -0400

    Add: Implement jwt and users permissions

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,6 +1,8 @@
 from accounts.serializers import UserSerializer
 from rest_framework import viewsets
 from accounts.models import User
+from rest_framework import permissions
+from accounts.permissions import IsPostOrOwnerOrReadOnly
 
 # Create your views here.
 
@@ -8,3 +10,5 @@ from accounts.models import User
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    permission_classes = (IsPostOrOwnerOrReadOnly, )
