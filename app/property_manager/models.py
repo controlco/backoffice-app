e59f6db97383786237bf288f7f800be2ee9410a6
jirarrazabal <jirarrazabal@uc.cl>
2021-04-28 00:40:47 -0400

    extend property model

--- app/property_manager/models.py
+++ app/property_manager/models.py
@@ -7,3 +7,9 @@ class Property(models.Model):
     title = models.TextField()
     owner = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="property")
+    surface = models.PositiveIntegerField(blank=True, null=True)
+    adress = models.TextField(blank=True, null=True)
+    price = models.PositiveIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    latitude = models.CharField(max_length=50, blank=True, null=True)
+    longitude = models.CharField(max_length=50, blank=True, null=True)
