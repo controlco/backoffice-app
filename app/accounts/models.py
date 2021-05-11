17edeb41499d2f6acff267ddf8936afeadf471c3
jirarrazabal <jirarrazabal@uc.cl>
2021-05-11 13:59:22 -0400

    add username field

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -6,9 +6,16 @@ from django.urls import reverse
 
 class User(AbstractUser):
     is_active = models.BooleanField(default=False, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True
+        )
     rut = models.CharField(max_length=10, blank=True, null=True)
     birth_date = models.DateTimeField(blank=True, null=True)
     is_owner = models.BooleanField(default=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
     
     def get_absolute_url(self):
         return reverse('user-detail', args=[str(self.id)])
