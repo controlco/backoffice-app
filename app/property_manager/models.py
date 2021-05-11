21ae53a0ff816efff56711ae2fe9d3d7c6b958ba
chris-carstens <ctcarstens@uc.cl>
2021-05-11 01:40:17 -0400

    Add Tests for Region and District

--- app/property_manager/models.py
+++ app/property_manager/models.py
@@ -3,6 +3,17 @@ from accounts.models import User
 # Create your models here.
 
 
+class Region(models.Model):
+    name = models.TextField()
+    number = models.PositiveIntegerField(primary_key=True)
+
+
+class District(models.Model):
+    name = models.TextField()
+    region = models.ForeignKey(
+        Region, on_delete=models.CASCADE, related_name="district")
+
+
 class Property(models.Model):
     title = models.TextField()
     owner = models.ForeignKey(
@@ -13,3 +24,5 @@ class Property(models.Model):
     description = models.TextField(blank=True, null=True)
     latitude = models.CharField(max_length=50, blank=True, null=True)
     longitude = models.CharField(max_length=50, blank=True, null=True)
+    district = models.ForeignKey(
+        District, on_delete=models.CASCADE, null=True, related_name="property")
