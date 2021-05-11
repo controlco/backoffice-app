e0cb876e238d64b738d3f76614a8a44f6b9329f1
jirarrazabal <jirarrazabal@uc.cl>
2021-05-10 23:16:29 -0400

    feat:add rut, birth_date, is_active and is_owner

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -4,4 +4,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    is_active = models.BooleanField(default=False, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')
+    rut = models.CharField(max_length=10, blank=True, null=True)
+    birth_date = models.DateTimeField(blank=True, null=True)
+    is_owner = models.BooleanField(default=True)
     pass
