b855bcb4c58959821da0c03aef95989f126b7ef3
jirarrazabal <jirarrazabal@uc.cl>
2021-05-10 23:28:53 -0400

    feat: add str and absolute email

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 # Create your models here.
 
 
@@ -8,4 +9,10 @@ class User(AbstractUser):
     rut = models.CharField(max_length=10, blank=True, null=True)
     birth_date = models.DateTimeField(blank=True, null=True)
     is_owner = models.BooleanField(default=True)
-    pass
+    
+    def get_absolute_url(self):
+        return reverse('user-detail', args=[str(self.id)])
+    
+    def __str__(self):
+        return self.email
+    
