f72d03c7af675f6c364c15cb23bdde45f7f11964
chris-carstens <ctcarstens@uc.cl>
2021-05-15 22:50:12 -0400

    Improve Seeds

--- app/app/settings.py
+++ app/app/settings.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
