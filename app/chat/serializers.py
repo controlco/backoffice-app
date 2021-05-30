1de6bc66db9b51e1c5dac71f3ee4fadab045e0b1
Christian Carstens <42195947+chris-carstens@users.noreply.github.com>
2021-05-30 19:26:09 -0400

    Fix/serializers messages (#54)
    * Fix seeds Factory
    
    * Update messages serializer

--- app/chat/serializers.py
+++ app/chat/serializers.py
@@ -3,6 +3,14 @@ from chat.models import Message
 
 
 class MessageSerializer(serializers.ModelSerializer):
+    from_user_first_name = serializers.ReadOnlyField(
+        source='from_user.first_name')
+    from_user_last_name = serializers.ReadOnlyField(
+        source='from_user.last_name')
+    to_user_first_name = serializers.ReadOnlyField(
+        source='to_user.first_name')
+    to_user_last_name = serializers.ReadOnlyField(
+        source='to_user.last_name')
     from_user_email = serializers.ReadOnlyField(source='from_user.email')
     from_id = serializers.ReadOnlyField(source='from_user.id')
     to_user_email = serializers.ReadOnlyField(source='to_user.email')
@@ -10,4 +18,6 @@ class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = ["id", "subject", "content", "from_user_email",
-                  "from_id", "to_user_email", "to_user", "read"]
+                  "from_id", "to_user_email", "to_user", "read",
+                  "from_user_first_name", "from_user_last_name",
+                  "to_user_first_name", "to_user_last_name"]
